0ed0b53efb7c56590e68c67562b4d77062796aa1
Derek Mason <3derekmason@gmail.com>
2022-03-23 11:44:40 -0600

    display glucose unit colors depending on value

--- ui/src/components/MainTile.jsx
+++ ui/src/components/MainTile.jsx
@@ -18,10 +18,14 @@ const getTime = timeString => {
   return hour + ':' + min + ' ' + ampm
 }
 
+const setValueColor = num => {
+  return num < 76 ? '#0d47a1' : num > 75 && num < 136 ? '#1b5e20' : num > 135 && num < 200 ? '#ff6f00' : '#b71c1c'
+}
+
 const MainTile = ({ data }) => {
   return (
     <Card className='mainTile'>
-      <h1>{data.value}</h1>
+      <h1 style={{ color: setValueColor(data.value) }}>{data.value}</h1>
       <div className='dateTime'>
         <h3>{getDate(data.date)}</h3>
         <h4>{getTime(data.date)}</h4>
