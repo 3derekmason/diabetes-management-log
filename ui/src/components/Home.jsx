137e0d46e6e592621a91d02ce3948eff37e14731
Derek Mason <3derekmason@gmail.com>
2022-03-23 07:21:21 -0600

    add appbar to home page

--- ui/src/components/Home.jsx
+++ ui/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Card, Paper } from '@mui/material'
+import { AppBar, Card, Paper } from '@mui/material'
 import { useEffect, useState } from 'react'
 
 import '../styles/Home.css'
@@ -6,9 +6,8 @@ import '../styles/Home.css'
 const Home = () => {
   return (
     <div>
-      <Card className='homePage' style={{ background: '#424242' }}>
-        Home Page
-      </Card>
+      <AppBar style={{ background: '#9e9e9e' }}>Home Page</AppBar>
+      <Card className='homePage' style={{ background: '#424242' }}></Card>
     </div>
   )
 }
