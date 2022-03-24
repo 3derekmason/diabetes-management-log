7a7dfbe8b778e1a5585a23eb577af02134881c02
Derek Mason <3derekmason@gmail.com>
2022-03-23 23:42:22 -0600

    create new entry form text fields

--- ui/src/components/CreateNew.jsx
+++ ui/src/components/CreateNew.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Box, Card, Modal, Paper, TextField, Toolbar, Typography } from '@mui/material'
+import { AppBar, Box, Button, Card, Modal, Paper, TextField, Toolbar, Typography } from '@mui/material'
 import { useContext, useEffect, useState } from 'react'
 
 import '../styles/CreateNew.css'
@@ -7,10 +7,28 @@ import AppContext from '../context'
 const CreateNew = ({ details }) => {
   return (
     <Modal open={details.createOpen} onClose={details.handleCreateClose} className='createNew'>
-      <Card className='createNewEntry'>
-        <form>
-          <TextField>Hello</TextField>
-        </form>
+      <Card style={{ background: '#424242', color: '#fff' }} className='createNewEntry'>
+        <Typography variant='h6' style={{ color: '#e0f7fa' }}>
+          What is your blood sugar?
+        </Typography>
+        <div className='formRow'>
+          <Typography variant='subtitle2'>Current glucose level: </Typography>
+          <TextField
+            type='number'
+            InputLabelProps={{
+              shrink: true
+            }}
+            size='small'
+            variant='filled'
+            style={{ width: '80px' }}
+            sx={{ input: { color: 'white' } }}
+          />
+        </div>
+        <Typography>Comments: </Typography>
+        <TextField placeholder='Ex: After meal' multiline variant='standard' style={{ width: '95%' }} />
+        <div className='formRow'>
+          <Button style={{ color: '#84ffff', marginTop: '16px' }}>Log Entry</Button>
+        </div>
       </Card>
     </Modal>
   )
