5d1bd63146731aee49234afb1d0badf8477c2f4e
Derek Mason <3derekmason@gmail.com>
2022-03-24 07:29:11 -0600

    delete entry ui side complete

--- ui/src/components/Delete.jsx
+++ ui/src/components/Delete.jsx
@@ -1,10 +1,19 @@
 import { AppBar, Box, Button, Card, Modal, Toolbar, Typography } from '@mui/material'
 import { useContext, useEffect, useState } from 'react'
+import client from '../client'
 
 import '../styles/DeleteModal.css'
 import AppContext from '../context'
 
 const Delete = ({ details }) => {
+  const { getAllEntries } = useContext(AppContext)
+  const deleteEntry = async id => {
+    await client
+      .delete('/entries', { data: { id } })
+      .then(getAllEntries())
+      .catch(err => console.log(err))
+    details.handleDeleteClose()
+  }
   return (
     <Modal open={details.deleteOpen} onClose={details.handleDeleteClose} className='detailModal'>
       <Box className='deleteView'>
@@ -13,7 +22,7 @@ const Delete = ({ details }) => {
           <Button variant='filled' style={{ color: '#121212' }} onClick={details.handleDeleteClose}>
             No
           </Button>
-          <Button variant='filled' style={{ color: '#D50000' }} onClick={() => console.log(details.id)}>
+          <Button variant='filled' style={{ color: '#D50000' }} onClick={deleteEntry}>
             Yes
           </Button>
         </div>
