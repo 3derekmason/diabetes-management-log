f377f8cc1ba7bf45326517eb9553e76758ba5992
Derek Mason <3derekmason@gmail.com>
2022-03-23 23:58:17 -0600

    form styling

--- ui/src/components/CreateNew.jsx
+++ ui/src/components/CreateNew.jsx
@@ -7,8 +7,8 @@ import AppContext from '../context'
 const CreateNew = ({ details }) => {
   return (
     <Modal open={details.createOpen} onClose={details.handleCreateClose} className='createNew'>
-      <Card style={{ background: '#424242', color: '#fff' }} className='createNewEntry'>
-        <Typography variant='h6' style={{ color: '#e0f7fa' }}>
+      <Card style={{ background: '#e0e0e0', color: '#121212' }} className='createNewEntry'>
+        <Typography variant='h6' style={{ color: '#212121' }}>
           What is your blood sugar?
         </Typography>
         <div className='formRow'>
@@ -21,13 +21,12 @@ const CreateNew = ({ details }) => {
             size='small'
             variant='filled'
             style={{ width: '80px' }}
-            sx={{ input: { color: 'white' } }}
           />
         </div>
         <Typography>Comments: </Typography>
         <TextField placeholder='Ex: After meal' multiline variant='standard' style={{ width: '95%' }} />
         <div className='formRow'>
-          <Button style={{ marginTop: '16px' }}>Log Entry</Button>
+          <Button style={{ marginTop: '16px', background: '#616161' }}>Log Entry</Button>
         </div>
       </Card>
     </Modal>
