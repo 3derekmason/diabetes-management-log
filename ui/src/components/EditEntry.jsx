24cfaef0b788cdc273e9d613d0504e4a1946b885
Derek Mason <3derekmason@gmail.com>
2022-03-31 09:49:25 -0600

    animate and style edit entry box

--- ui/src/components/EditEntry.jsx
+++ ui/src/components/EditEntry.jsx
@@ -1,12 +1,11 @@
 import { Box, Button, Modal, Typography } from '@mui/material'
-import { useContext, useState } from 'react'
 
-import AppContext from '../context'
+import '../styles/EditEntry.css'
 
 const EditEntry = ({ util }) => {
   return (
     <Modal open={util.editOpen} onClose={util.handleEditClose} className='editModal'>
-      <Box>
+      <Box className='editBox'>
         <Button>TEST</Button>
       </Box>
     </Modal>
